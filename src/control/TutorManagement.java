51363a424768630655ba70321d23132d779cebf3
YIPZIYAN <103913961+YIPZIYAN@users.noreply.github.com>
2023-08-31 22:38:18 +0800

    debug DAO

--- src/control/TutorManagement.java
+++ src/control/TutorManagement.java
@@ -18,20 +18,20 @@ import utility.MessageUI;
  * @author Yip Zi Yan
  */
 public class TutorManagement {
-
+    
     private final String fileName = "tutor.dat";
     DAO tutorDAO = new DAO(fileName);
     TutorManagementUI tutorUI = new TutorManagementUI();
     ListInterface<Tutor> tutorList = new CircularDoublyLinkedList<>();
 
-//    Seeder seeder = new Seeder(); 
-
+    Seeder seeder = new Seeder(); 
     public TutorManagement() {
         tutorList = tutorDAO.retrieveFromFile();
+        Tutor.setTotalTutor(tutorList.getNumberOfEntries());
     }
-
+    
     public void startUI() {
-
+        
         int choice;
         do {
             choice = tutorUI.getMenuChoice();
@@ -52,48 +52,48 @@ public class TutorManagement {
                     MessageUI.displayExitMessage();
             }
         } while (choice != 0);
-
+        
     }
-
+    
     public static void main(String[] args) {
         TutorManagement teachingControl = new TutorManagement();
         teachingControl.startUI();
     }
-
+    
     private void addNewTutor() {
         if (!tutorUI.addTutorMenu()) {
             return;
         }
-
+        
         do {
             Tutor newTutor = tutorUI.addTutor();
-
+            
             if (newTutor != null) {
                 tutorList.add(newTutor);
                 tutorDAO.saveToFile(tutorList);
             }
-
+            
         } while (tutorUI.contAction("Anymore To Add? [Y|N] > "));
-
+        
     }
-
+    
     private void displayTutorList() {
         tutorUI.displayAllTutor(getAllTutor(), true);
     }
-
+    
     private String getAllTutor() {
         String outputStr = "";
         int number = 0;
-
+        
         Iterator<Tutor> it = tutorList.getIterator();
         while (it.hasNext()) {
             outputStr += String.format("%2d.  ", ++number)
                     + it.next() + "\n";
-
+            
         }
         return outputStr;
     }
-
+    
     private void searchTutorUI() {
         int choice;
         do {
@@ -114,14 +114,14 @@ public class TutorManagement {
             }
         } while (choice != 0);
     }
-
+    
     private void searchTutorBy(String attribute, String queryQuestion) {
         String query = tutorUI.getTutorQuery(queryQuestion, "find")
                 .toLowerCase();
         String outputStr = "";
         Iterator<Tutor> it = tutorList.getIterator();
         int number = 0;
-
+        
         while (it.hasNext()) {
             Tutor matchTutor = it.next();
             switch (attribute) {
@@ -144,13 +144,13 @@ public class TutorManagement {
                     }
                     break;
             }
-
+            
         }
-
+        
         tutorUI.displayFindResult(outputStr);
-
+        
     }
-
+    
     private void updateTutorUI() {
         int choice;
         do {
@@ -163,26 +163,26 @@ public class TutorManagement {
                 case 2:
                     updateTutor("id", "Enter Tutor ID > ");
                     break;
-
+                
             }
         } while (choice != 0);
     }
-
+    
     private void updateTutor(String searchBy, String queryQuestion) {
         String query = tutorUI.getTutorQuery(queryQuestion, "update")
                 .toLowerCase();
         Tutor updateTutor = null;
         Iterator<Tutor> it = tutorList.getIterator();
-
+        
         switch (searchBy) {
             case "id":
                 while (it.hasNext()) {
                     Tutor matchTutor = it.next();
-
+                    
                     if (matchTutor.getTutorId().toLowerCase().equals(query)) {
                         updateTutor = matchTutor;
                     }
-
+                    
                 }
                 break;
             case "num":
@@ -193,11 +193,11 @@ public class TutorManagement {
             }
             break;
         }
-
+        
         updateSelectedField(updateTutor);
-
+        
     }
-
+    
     private void updateSelectedField(Tutor updateTutor) {
         int choice;
         do {
@@ -205,7 +205,7 @@ public class TutorManagement {
             if (updateTutor == null) {
                 return;
             }
-
+            
             choice = tutorUI.selectAttributeToUpdate(updateTutor);
             switch (choice) {
                 case 1:
@@ -220,40 +220,40 @@ public class TutorManagement {
             }
         } while (choice != 0);
     }
-
+    
     private void updateTutorName(Tutor updateTutor) {
         String newName = tutorUI.updateTutorName();
         if (newName == null) {
             return;
         }
-
+        
         updateTutor.setTutorName(newName);
         tutorDAO.saveToFile(tutorList);
     }
-
+    
     private void updateTutorGender(Tutor updateTutor) {
         boolean confirmToChange = tutorUI.updateTutorGender(updateTutor.isFemale());
         if (!confirmToChange) {
             return;
         }
-
+        
         if (updateTutor.isFemale()) {
             updateTutor.setGender('M');
         } else {
             updateTutor.setGender('F');
         }
         tutorDAO.saveToFile(tutorList);
-
+        
     }
-
+    
     private void updateTutorStatus(Tutor updateTutor) {
         String newStatus = tutorUI.updateTutorStatus(updateTutor.getStatus());
         if (newStatus == null) {
             return;
         }
-
+        
         updateTutor.setStatus(newStatus);
         tutorDAO.saveToFile(tutorList);
-
+        
     }
 }
